26903c3a5a99f546a275dfefd5880455664bcbef
Wyatt Monroe <wyatt.monroe2013@gmail.com>
2026-06-06 21:39:20 -0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -16,25 +16,12 @@ import {
 }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-/* ================= FIREBASE CONFIG ================= */
+import { firebaseConfig } from "./firebase-config.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBLghWLth0syJhARDMWiJ7xNwyJAh7MWjQ",
-  authDomain: "securly-plans-main.firebaseapp.com",
-  projectId: "securly-plans-main",
-  storageBucket: "securly-plans-main.firebasestorage.app",
-  messagingSenderId: "613499545769",
-  appId: "1:613499545769:web:baa071714434c4814de1b8"
-};
-
-/* ================= INITIALIZE APP ================= */
+/* ================= INIT ================= */
 
 export const app = initializeApp(firebaseConfig);
 
-/* ================= FIREBASE SERVICES ================= */
-
 export const db = getFirestore(app);
-
 export const storage = getStorage(app);
-
 export const auth = getAuth(app);
