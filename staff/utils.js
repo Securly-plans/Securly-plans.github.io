1911c7bb40fcaea9ac85e57f06b7acf23522ea1e
Wyatt Monroe <wyatt.monroe2013@gmail.com>
2026-06-14 16:18:21 -0700

    Update and rename login.html to utils.js

---
+++ staff/utils.js
@@ -0,0 +1,20 @@
+import { db, doc, getDoc, setDoc } from "./firebase.js";
+
+export async function generateStaffId() {
+  const metaRef = doc(db, "system", "meta");
+  const snap = await getDoc(metaRef);
+
+  let last = 0;
+
+  if (snap.exists()) {
+    last = snap.data().lastStaffId || 0;
+  }
+
+  const newId = last + 1;
+
+  await setDoc(metaRef, {
+    lastStaffId: newId
+  }, { merge: true });
+
+  return String(newId).padStart(3, "0");
+}
