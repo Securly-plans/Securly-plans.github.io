2841b1bc92478a34365252a0bd15a299efb94757
Securly-plans <sethcraft14@hotmail.com>
2026-07-21 21:51:08 -0700

    Refactor PokiSDK implementation for better structure

--- games/monkey-mart/patch/js/poki-sdk.js
+++ games/monkey-mart/patch/js/poki-sdk.js
@@ -1,33 +1,24 @@
-// Safe PokiSDK Replacement
-(function() {
+(function () {
     'use strict';
-
-    var dummySDK = {
-        init: function() { return Promise.resolve(true); },
-        initWithPoki: function() { return Promise.resolve(true); },
-        setDebug: function() {},
-        gameLoadingStart: function() {},
-        gameLoadingProgress: function() {},
-        gameLoadingFinished: function() {},
-        gameplayStart: function() {},
-        gameplayStop: function() {},
-        happyTime: function() {},
-        displayAd: function() { return Promise.resolve(true); },
-        destroyAd: function() {},
-        rewardedBreak: function() { return Promise.resolve(true); },
-        commercialBreak: function() { return Promise.resolve(true); },
-        customEvent: function() {},
-        captureError: function() {},
-        isAdBlocked: function() { return false; }
+    window.PokiSDK = {
+        init: function () { return Promise.resolve(true); },
+        initWithPoki: function () { return Promise.resolve(true); },
+        gameLoadingStart: function () {},
+        gameLoadingProgress: function () {},
+        gameLoadingFinished: function () {},
+        gameplayStart: function () {},
+        gameplayStop: function () {},
+        happyTime: function () {},
+        setDebug: function () {},
+        isAdBlocked: function () { return false; },
+        displayAd: function (container, size, callback) {
+            if (typeof callback === 'function') callback();
+            return Promise.resolve(true);
+        },
+        destroyAd: function () {},
+        rewardedBreak: function () { return Promise.resolve(true); },
+        commercialBreak: function () { return Promise.resolve(true); },
+        shareableURL: function () { return Promise.resolve(""); },
+        getURLParam: function () { return ""; }
     };
-
-    // Safety net: Proxy catches any missing functions so the game never crashes
-    window.PokiSDK = new Proxy(dummySDK, {
-        get: function(target, prop) {
-            if (prop in target) {
-                return target[prop];
-            }
-            return function() { return Promise.resolve(true); };
-        }
-    });
 })();
